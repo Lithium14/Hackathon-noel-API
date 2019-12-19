208a20b60b494b4cdfa34b7317cc4d69ecb819b8
Lithium14 <fadi.rata@live.fr>
2019-12-20 00:26:12 +0100

    begin socket

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,32 @@ import express from 'express';
 import { SportController } from './controller/sport.controller';
 import 'reflect-metadata';
 import loaders from './loaders';
+import { Server } from 'http';
+import io from 'socket.io';
+
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
+
+app.get('/', function(req: Request, res: Response) {
+  res.sendFile(__dirname + '/index.html');
+});
 
 async function startServer() {
     // Récupération de l'application initiale
     const app = express();
+    const server = new Server(app);
+    const socke = io(server);
+    socke.onconnection((userSock: io.Server) => {
+      console.log('Connextion detecté');
+
+      userSock.emit('nes message');
+
+      userSock.on('message', (data: any) => {
+        console.log(data);
+
+      });
+
+    });
 
     // Chargement des différent loader
     await loaders(app);
@@ -26,3 +48,7 @@ async function startServer() {
   }
 
 startServer();
+
+// server.listen(port, () => {
+//   console.log(`started on port: ${port}`);
+// });
