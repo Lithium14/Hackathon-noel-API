0c8d99dadbe86c771887aecc7cede69471274770
Lithium14 <fadi.rata@live.fr>
2019-12-20 11:28:47 +0100

    socket good

--- src/index.ts
+++ src/index.ts
@@ -9,46 +9,127 @@ import loaders from './loaders';
 import { Server } from 'http';
 import io from 'socket.io';
 
-server.listen(80);
-// WARNING: app.listen(80) will NOT work here!
-
-app.get('/', function(req: Request, res: Response) {
-  res.sendFile(__dirname + '/index.html');
-});
-
 async function startServer() {
-    // Récupération de l'application initiale
-    const app = express();
-    const server = new Server(app);
-    const socke = io(server);
-    socke.onconnection((userSock: io.Server) => {
-      console.log('Connextion detecté');
+  // Récupération de l'application initiale
+  const app = require('express')();
+  const server = require('http').Server(app);
+  const io = require('socket.io')(server);
 
-      userSock.emit('nes message');
+  // const server = http.createServer(app);
 
-      userSock.on('message', (data: any) => {
-        console.log(data);
+  const debug = require('debug')('angular2-nodejs:server');
 
-      });
+    /**
+     * Get port from environment and store in Express.
+     */
 
+    /**
+     * Create HTTP server.
+     */
+
+  // const io = require('socket.io').listen(server);
+
+  io.on('connection', (socket: any) => {
+
+        console.log('new connection made.');
+
+        socket.on('join', function(data: any) {
+          // joining
+          socket.join(data.room);
+
+          console.log(data.user + 'joined the room : ' + data.room);
+
+          socket.broadcast.to(data.room).emit('new user joined', {user: data.user, message: 'has joined this room.'});
+        });
+
+        socket.on('leave', function(data: any) {
+
+          console.log(data.user + 'left the room : ' + data.room);
+
+          socket.broadcast.to(data.room).emit('left room', {user: data.user, message: 'has left this room.'});
+
+          socket.leave(data.room);
+        });
+
+        socket.on('message', function(data: any) {
+
+          io.in(data.room).emit('new message', {user: data.user, message: data.message});
+        });
     });
 
+    /**
+     * Listen on provided port, on all network interfaces.
+     */
+
+    /**
+     * Normalize a port into a number, string, or false.
+     */
+
+  function normalizePort(val: any) {
+      const port = parseInt(val, 10);
+
+      if (isNaN(port)) {
+        // named pipe
+        return val;
+      }
+
+      if (port >= 0) {
+        // port number
+        return port;
+      }
+
+      return false;
+    }
+
+    /**
+     * Event listener for HTTP server "error" event.
+     */
+
+  function onError(error: any) {
+      if (error.syscall !== 'listen') {
+        throw error;
+      }
+
+      // handle specific listen errors with friendly messages
+      switch (error.code) {
+        case 'EACCES':
+          console.error('ÀÀ' + ' requires elevated privileges');
+          process.exit(1);
+          break;
+        case 'EADDRINUSE':
+          console.error('SOSPOS' + ' is already in use');
+          process.exit(1);
+          break;
+        default:
+          throw error;
+      }
+    }
+
+    /**
+     * Event listener for HTTP server "listening" event.
+     */
+
+  function onListening() {
+      const addr: any = server.address();
+      const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+      debug('Listening on ' + bind);
+    }
     // Chargement des différent loader
-    await loaders(app);
+  await loaders(app);
 
     // Ajout des différentes route de votre application
-    SportController(app);
-    const userController = new UserController(app);
-    const messageController = new MessageController(app);
-    const scoreContrller = new ScoreController(app);
-    const worldController = new WorldController(app);
+  SportController(app);
+  const userController = new UserController(app);
+  const messageController = new MessageController(app);
+  const scoreContrller = new ScoreController(app);
+  const worldController = new WorldController(app);
 
     // Démarrage du serveur une fois que tout est correctement init
-    app.listen(3000, () => console.log('Express server  is running'));
+
+  server.listen(1234);
   }
 
 startServer();
 
-// server.listen(port, () => {
-//   console.log(`started on port: ${port}`);
-// });
